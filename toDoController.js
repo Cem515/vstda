d13d3aea3eb8655b9adb1e8c33ae10d9a954fe90
Collin McCoy <collinmccoy17@gmail.com>
2017-05-01 14:52:22 -0700

    next step

---
+++ toDoController.js
@@ -0,0 +1,35 @@
+
+(function() {
+  'use strict';
+
+  angular
+    .module('toDoList')
+    .controller('ToDoController', ToDoController);
+
+  ToDoController.$inject = [];
+
+  /* @ngInject */
+  function ToDoController() {
+
+
+    var vm = this;
+    var value = vm.priorities
+    vm.toDoItem = "";
+    vm.priorities = 1;
+    vm.toDoList = [];
+    vm.sort = 'value';
+    vm.reverse = false;
+
+
+    vm.addItem = function() {
+
+      vm.toDoList.push(vm.toDoObject.toDoItem);
+      console.log(vm.toDoObject);
+    }
+
+    vm.sortBy = function(){
+      
+
+    }
+  }
+})();
