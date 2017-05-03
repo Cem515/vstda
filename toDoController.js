c8845f4eeac81550638224dcdfe530d67d390c69
Collin McCoy <collinmccoy17@gmail.com>
2017-05-03 09:25:54 -0700

    finished

--- toDoController.js
+++ toDoController.js
@@ -17,41 +17,19 @@
 
   /* @ngInject */
   function ToDoController() {
-
+// Define Empty Variables
     var vm = this;
     vm.toDoItem = "";
     vm.toDoList = [];
     vm.toDoObject = [];
-    vm.sort = 'value';
-    vm.reverse = false;
 
+// CLick Add Items to List
     vm.addItem = function() {
 
       vm.toDoList.push({
         toDo: vm.toDoObject.toDoItem,
         priority: vm.toDoObject.priorities
       });
-    }
-
-    vm.prior = function(priorities) {
-      {
-        var priorities = vm.toDoObject.priorities
-        console.log(priorities);
-        var swapped;
-        do {
-          swapped=false;
-          for(var i=0; i<priorities.length-1; i++) {
-            if (a[i] > a[i+1]) {
-              var temp = a[i];
-              a[i] = a[i+1];
-              a[i+1] = temp;
-              swapped = true;
-            }
-          }
-        } while (swapped);
-      }
-vm.prior(priorities);
-console.log(priorities);
     }
   }
 })();
